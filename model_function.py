28a24609ce76cbd00c6b0805324a196428582650
Daniel Cierpinsky <daniel.cierpinsky@gmail.com>
2023-11-24 19:58:00 +0100

    exponential in loss, unstable

--- model_function.py
+++ model_function.py
@@ -2,8 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def double_exponential_function( t: np.ndarray, A1:float, A2: float, t1: float, t2: float,toff1: float, toff2: float, j0: float )->np.ndarray:
-    """Evaluate double exponential function A1*e^(t/t1)+A2*e^(t/t2)+j0 over the array of t with given parameters.
+def double_exponential_function( t: np.ndarray,parameter_array: np.ndarray )->np.ndarray:
+    """Evaluate double exponential function A1*e^(t*t1)+A2*e^(t*t2)+j0 over the array of t with given parameters.
     Returns an array with the same shape as t. Function value is calculated element-wise."""
-    return A1*np.exp(-(t+toff1)/t1)+A2*np.exp(-(t+toff2)/t2)+j0
+    assert parameter_array.shape == (5,) , f"shape of parameter array is {parameter_array.shape}, expected {(5,)}"
+    A1,t1,A2,t2,j0 = parameter_array
+
+    return A1*np.exp(-(t)*t1)+A2*np.exp(-(t)*t2)+j0
 
